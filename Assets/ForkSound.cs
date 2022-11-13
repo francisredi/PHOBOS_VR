45d3a91882550a029bdf8ecfd47c6d24c6b48d1a
Francisco Rojas <francisrojasphd@gmail.com>
2022-11-12 20:52:41 -0500

    Remainder

---
+++ Assets/ForkSound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForkSound : MonoBehaviour {
+
+	public AudioSource audio;
+	public AudioClip forkSoundClip;
+
+
+	void OnCollissionEnter (Collision col) {
+
+		if (col.gameObject.name == "Fork" && col.gameObject.tag == "Prop") {
+							
+				//GetComponent<AudioSource> () = GetComponent<AudioSource> ();
+				//GetComponent<AudioSource> ().Play ();
+			AudioSource audio = GetComponent<AudioSource>();
+			audio.Play ();
+
+			}
+		}
+
+	}
